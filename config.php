3486e710913d1f879005be6ed346935acf6b7cb7
ediibraimi <ediibraimi9@gmail.com>
2024-08-12 18:56:19 +0200

    config.php created

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+$host = 'localhost';  
+$dbname = 'farm_unity';  
+$username = 'root';  
+$password = '';  
+try {
+    // Create a new PDO instance
+    $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    
+    // Set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    // Handle connection errors
+    echo "Connection failed: " . $e->getMessage();
+    die();
+}
+?>
