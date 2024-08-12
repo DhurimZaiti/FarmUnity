6b01f322898c193e866e349be817ad6b7a4d4ebb
ediibraimi <ediibraimi9@gmail.com>
2024-08-12 20:01:07 +0200

    logic done and dummy data done

---
+++ loginLogic.php
@@ -0,0 +1,44 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+include_once('config.php');
+session_start();
+
+if (isset($_POST['submit'])) {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    // Prepare SQL statement
+    $sql = "SELECT * FROM users WHERE email = :email";
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam(':email', $email);
+
+    if ($stmt->execute()) {
+        $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if ($user && password_verify($password, $user['password'])) {
+            $_SESSION['farm_unity_userId'] = $user['id'];
+            $_SESSION['farm_unity_username'] = $user['username'];
+            $_SESSION['farm_unity_user_email'] = $user['email'];
+
+            // Redirect to the dashboard or home page
+            header("Location: testpage.php");
+            exit();
+        } else {
+            $_SESSION['login_error_message'] = "Invalid email or password.";
+            header("Location: login.php");
+            exit();
+        }
+    } else {
+        $_SESSION['login_error_message'] = "Error executing the query: " . implode(", ", $stmt->errorInfo());
+        header("Location: login.php");
+        exit();
+    }
+} else {
+    $_SESSION['login_error_message'] = "Invalid request.";
+    header("Location: login.php");
+    exit();
+}
+?>
