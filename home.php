ccf24cbd1796d346d9a4d0095f20eb7b9fae2d96
DhurimZaiti <dhurim.work@gmail.com>
2024-08-15 13:35:35 +0200

    ijoji

---
+++ home.php
@@ -0,0 +1,25 @@
+<?php 
+    include_once('header.php');
+?>
+<div class="contents">
+    <div class="container">
+        <div class="content">
+            <div class="row">
+                <div class="col-12">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="card-link">Card link</a>
+                        <a href="#" class="card-link">Another link</a>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php 
+    include_once('footer.php');
+?>
