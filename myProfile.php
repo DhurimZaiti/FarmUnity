f1b0a4e68006d9c4ae5443950bfa99e1dbb5154d
DhurimZaiti <dhurim.work@gmail.com>
2024-08-16 14:39:31 +0200

    afsdfsf

---
+++ myProfile.php
@@ -0,0 +1,13 @@
+<?php 
+    include_once('header.php');
+?>
+<div class="contents">
+    <div class="container">
+        <div class="content">
+
+        </div>
+    </div>
+</div>
+<?php 
+    include_once('footer.php');
+?>
