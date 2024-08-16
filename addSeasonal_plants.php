593ec16224464c781e10e09b621b6fb47835ae88
ArtinaHaziri <artinahaziri@gmail.com>
2024-08-16 14:34:43 +0200

    link changes bootstrap

--- addSeasonal_plants.php
+++ addSeasonal_plants.php
@@ -59,7 +59,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Add Seasonal Plant</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
     <style>
         body {
             background-color: #f8f9fa; /* Optional: Light background color for the body */
