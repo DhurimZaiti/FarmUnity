eb3f6af1b4d8622687fb4e1cc520a6dd30b2daf6
DhurimZaiti <dhurim.work@gmail.com>
2024-08-25 22:46:53 +0200

    rewewfsd
    dsfsd

--- singleAnimal.php
+++ singleAnimal.php
@@ -1,3 +1,60 @@
 <?php 
     include_once('header.php');
 ?>
+<div class="content">
+    <div class="container">
+        <div class="contents">
+            <div class="row">
+                <div class="col-12">
+                    <h4 class="h4">General Info</h4>
+                    <hr class="bg-dark" style="margin-right: 420px;">
+                    <div class="ms-3" id="layout-body">
+                        <p class="h6 mb-1">Name:<span class="ms-2">Bessie</span></p>
+                        <p class="h6 mb-1">Species:<span class="ms-2">Cow</span></p>
+                        <p class="h6 mb-1">Breed:<span class="ms-2">Busha</span></p>
+                        <p class="h6 mb-1">Gender:<span class="ms-2">Female</span></p>
+                        <p class="h6 mb-1">Description:<span class="ms-2">Solid Cow, Beautiful, Very Economical :))</span></p>
+                    </div>
+
+                    <h4 class="h4">Metrics</h4>
+                    <hr class="bg-dark" style="margin-right: 420px;">
+                    <div class="ms-3" id="layout-body">
+
+                    </div>
+
+                    <h4 class="h4">Health</h4>
+                    <hr class="bg-dark" style="margin-right: 420px;">
+                    <div class="ms-3" id="layout-body">
+
+                    </div>
+
+                    <h4 class="h4">Living Conditions</h4>
+                    <hr class="bg-dark" style="margin-right: 420px;">
+                    <div class="ms-3" id="layout-body">
+
+                    </div>
+
+                    <h4 class="h4">Extra Information</h4>
+                    <hr class="bg-dark" style="margin-right: 420px;">
+                    <div class="ms-3" id="layout-body">
+
+                    </div>
+
+                    <h4 class="h4">Animal's Residence</h4>
+                    <hr class="bg-dark" style="margin-right: 420px;">
+                    <div class="ms-3" id="layout-body">
+
+                    </div>
+
+                    <h4 class="h4">Other Information</h4>
+                    <hr class="bg-dark" style="margin-right: 420px;">
+                    <div class="ms-3" id="layout-body">
+
+                    </div>
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
