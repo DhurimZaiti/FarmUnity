79a1bbe9b98771dd8bd8f1e02bdfd0a2c498ab02
Dhurim Zaiti <dhurim.work@gmail.com>
2024-09-01 20:18:39 +0200

    Udate 2 of single planting
    sfsdf

--- singlePlanting.php
+++ singlePlanting.php
@@ -18,7 +18,6 @@ include_once "header.php";
                         <p class="h6 mb-1 text-primary">Name:<span class="ms-2 text-dark">Corn</span></p>
                         <p class="h6 mb-1 text-primary">Origin:<span class="ms-2 text-dark">Corn</span></p>
                         <p class="h6 mb-1 text-primary">Age:<span class="ms-2 text-dark">Corn</span></p>
-                        <p class="h6 mb-1 text-primary">Soil Type:<span class="ms-2 text-dark">Corn</span></p>
                         <p class="h6 mb-1 text-primary">Planted at (Date)<!-- Calendar -->:<span class="ms-2 text-dark">Corn</span></p>
                         <p class="h6 mb-1 text-primary">Planted at (Field)<!-- Dropdown -->:<span class="ms-2 text-dark">Corn</span></p>
                         <p class="h6 mb-1 text-primary">Watering Cycle:<span class="ms-2 text-dark">Corn</span></p>
@@ -26,18 +25,59 @@ include_once "header.php";
                         <p class="h6 mb-1 text-primary">Growth Rate:<span class="ms-2 text-dark">Corn</span></p>
                         <p class="h6 mb-1 text-primary">Height:<span class="ms-2 text-dark">Corn</span></p>
                         <p class="h6 mb-1 text-primary">Spread:<span class="ms-2 text-dark">Corn</span></p>
-                        <p class="h6 mb-1 text-primary">Sun Requirements:<span class="ms-2 text-dark">Corn</span></p>
                         <p class="h6 mb-1 text-primary">Notes:<span class="ms-2 text-dark">Corn</span></p>
                         <p class="h6 mb-1 text-primary">Expected Harvest Time:<span class="ms-2 text-dark">Corn</span></p>
                         <p class="h6 mb-1 text-primary">Expected Yield:<span class="ms-2 text-dark">Corn</span></p>
                         <p class="h6 mb-1 text-primary">Actual Yield:<span class="ms-2 text-dark">Corn</span></p>
+                                            </div>
+                </div>
+                <div class="col-12">
+                    <h4 class="h4 mb-0">Soil/Sun Conditions</h4>
+                    <hr class="bg-dark" style="margin-right: 420px;">
+                    <div class="ms-3" id="layout-body">
+                        <p class="h6 mb-1 text-primary">Soil Type:<span class="ms-2 text-dark">Corn</span></p>
+                        <p class="h6 mb-1 text-primary">Sun Requirements:<span class="ms-2 text-dark">Corn</span></p>
                         <p class="h6 mb-1 text-primary">Pest/Disease Resistance:<span class="ms-2 text-dark">Corn</span></p>
                         <p class="h6 mb-1 text-primary">Fertilizer Requirements:<span class="ms-2 text-dark">Corn</span></p>
                         <p class="h6 mb-1 text-primary">Ideal Soil pH level:<span class="ms-2 text-dark">Corn</span></p>
                         <p class="h6 mb-1 text-primary">Spacing:<span class="ms-2 text-dark">Corn</span></p>
                     </div>
                 </div>
-
+                <div class="col-12">
+                    <h4 class="h4 mb-0">General Info</h4>
+                    <hr class="bg-dark" style="margin-right: 420px;">
+                    <div class="ms-3" id="layout-body">
+                        <p class="h6 mb-1 text-primary">Name:<span class="ms-2 text-dark">Corn</span></p>
+                    </div>
+                </div>
+                <div class="col-12">
+                    <h4 class="h4 mb-0">General Info</h4>
+                    <hr class="bg-dark" style="margin-right: 420px;">
+                    <div class="ms-3" id="layout-body">
+                        <p class="h6 mb-1 text-primary">Name:<span class="ms-2 text-dark">Corn</span></p>
+                    </div>
+                </div>
+                <div class="col-12">
+                    <h4 class="h4 mb-0">General Info</h4>
+                    <hr class="bg-dark" style="margin-right: 420px;">
+                    <div class="ms-3" id="layout-body">
+                        <p class="h6 mb-1 text-primary">Name:<span class="ms-2 text-dark">Corn</span></p>
+                    </div>
+                </div>
+                <div class="col-12">
+                    <h4 class="h4 mb-0">General Info</h4>
+                    <hr class="bg-dark" style="margin-right: 420px;">
+                    <div class="ms-3" id="layout-body">
+                        <p class="h6 mb-1 text-primary">Name:<span class="ms-2 text-dark">Corn</span></p>
+                    </div>
+                </div>
+                <div class="col-12">
+                    <h4 class="h4 mb-0">General Info</h4>
+                    <hr class="bg-dark" style="margin-right: 420px;">
+                    <div class="ms-3" id="layout-body">
+                        <p class="h6 mb-1 text-primary">Name:<span class="ms-2 text-dark">Corn</span></p>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
